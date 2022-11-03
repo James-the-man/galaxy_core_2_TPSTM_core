1b0dff9bea91394cf0a92713793c2e67b557cee2
Paul <81930845+James-the-man@users.noreply.github.com>
2022-11-03 20:14:48 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,4 @@
 @IF EXIST tools\bin SET PATH=%PATH%;tools\bin
  @if "%1" == "check" exit 0
  @if "%1" == "clean" exit 0
- @C:\Users\Pavlusha\Desktop\PhoneBakery\ABE\jdk-11\bin\java.exe -jar bbootimg/bbootimg.jar %*
+ @java.exe -jar bbootimg/bbootimg.jar %*
